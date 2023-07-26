8a31ffb4bb75a1dfaa14497685b184026fdbb6b0
Hasan Erken <hasan.erken@gmail.com>
2023-07-26 22:34:44 +0300

    naive, tailwindcss added

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
+<script lang="ts" setup>
+import type { ThemeConfig } from '@bg-dev/nuxt-naiveui'
+import { theme } from '#tailwind-config'
+
+const themeConfig: ThemeConfig = {
+  shared: {
+    common: {
+      fontFamily: theme.fontFamily.sans.join(', '),
+      lineHeight: theme.lineHeight.normal,
+    },
+  },
+  light: {
+    common: {
+      primaryColor: theme.colors.blue[600],
+      primaryColorHover: theme.colors.blue[500],
+      primaryColorPressed: theme.colors.blue[700],
+    },
+  },
+  dark: {
+    common: {
+      primaryColor: theme.colors.blue[500],
+      primaryColorHover: theme.colors.blue[400],
+      primaryColorPressed: theme.colors.blue[600],
+    },
+  },
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NaiveConfig :theme-config="themeConfig">
+      <NuxtPage />
+    </NaiveConfig>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Index Page
+    <n-button type="primary">
+      Merhaba
+    </n-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
